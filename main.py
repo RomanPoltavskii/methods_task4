12df2e86c953fbcb552b459b183a47319e7dd7d7
origin <romik.poltavskii.v@mail.ru>
2023-04-12 07:45:56 +0300

    small changes

--- main.py
+++ main.py
@@ -5,22 +5,18 @@ def f(x):
 
 a, b = 1, 3  # границы интервала
 n = 1000      # число интервалов (шагов сетки)
-dx = (b - a) / n  # шаг сетки
 
 # вычисляем значение интеграла методом правых прямоугольников
-integral = 0
-for i in range(n):
-    xi = a + i*dx
-    fi = f(xi + dx)
-    integral += fi*dx
+def left_rectangular_integration(f, a, b, n):
+    h = (b - a) / n
+    I = 0
 
-# вычисляем абсолютную и относительную погрешности
-I_exact = 1.51811  # точное значение интеграла
-I_approx = integral  # приближенное значение интеграла
-Delta_I_abs = abs(I_exact - I_approx)
-Delta_I_rel = Delta_I_abs / abs(I_exact)
+    for i in range(n):
+        x = a + i * h
+        I += f(x)
 
+    return h * I
+
+res = left_rectangular_integration(f,a,b,n)
 # выводим результаты
-print("Приближенное значение интеграла: ", I_approx)
-print("Абсолютная погрешность: ", Delta_I_abs)
-print("Относительная погрешность: ", Delta_I_rel)
+print("Приближенное значение интеграла: ", res)
